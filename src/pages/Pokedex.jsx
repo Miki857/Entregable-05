0ebf1fc70ab0bcdb53026359afbd16936f308423
Miguel <miguelcastillov175@gmail.com>
2024-04-29 22:10:24 -0500

    Second Commit

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,15 +2,15 @@ import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react'
 import PokemonCard from '../components/PokemonCard';
 import Pagination from '../components/Pagination';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { changeID } from "../store/slices/pokemon.slice";
 import { useNavigate } from 'react-router-dom';
 
 //STYLES//
 import "./css/pokedex.css"
 
-const Pokedex = ({credentials, pokemonTypes, }) => {
-    const pokemonId = useSelector((store) => store.pokemonId);
+const Pokedex = ({credentials, pokemonTypes}) => {
+    const [loading, setLoading] = useState(true);
     const [pokemones, setPokemones] = useState();
     const [limit, setLimit] = useState(5);
     const [offset, setOffset] = useState(0);
@@ -93,7 +93,8 @@ const Pokedex = ({credentials, pokemonTypes, }) => {
                         .then(res => {
                             setPokemonesData(pokemonesData2 => [...pokemonesData2, res.data]);//AQUI ESTAN SON SUS DATOS COMPLETOS.
                         })//SOLO CONSEGUIMOS SUS NOMBRES Y LA API DE SU INFO.
-                        .catch(err => console.log(err));
+                        .catch(err => console.log(err))
+                        .finally(setLoading(false));
                 }
             }
         }
@@ -114,48 +115,49 @@ const Pokedex = ({credentials, pokemonTypes, }) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        //CARGAMOS LOS POKEMONES A LA LISTA DINAMICA:
-        totalPokemonesApi?.forEach((el) => {
-            const li = document.createElement("li");
-            li.innerText = el.name.toUpperCase();
-            li.classList.add("class", "cursor-pointer");
-            
-            li.addEventListener("click", () => {
-                inputPokemon.current.value = el.name.toUpperCase();
-                dispatch(changeID(el.url.split("https://pokeapi.co/api/v2/pokemon/")[1].split("/")[0]))//CANMBIAMOS EL 'pokemonId'(Variable Global).
-                //OCULTAMOS TODOS LOS <li>:
-                document.querySelectorAll('#list li').forEach((li) => {
-                    li.hidden =true;
-                })
-            });
-
-            li.hidden = true;//LOS OCULTAMOS. APARECERAN CUANDO EL USARIO TIPEE ALGO EN EL INPUT.
-
-            //LOS AGREGAMOS AL UL:
-            ulPokemon.current.appendChild(li);
-        });
+        if(!loading){
+            //CARGAMOS LOS POKEMONES A LA LISTA DINAMICA:
+            totalPokemonesApi?.forEach((el) => {
+                const li = document.createElement("li");
+                li.innerText = el.name.toUpperCase();
+                li.classList.add("class", "cursor-pointer");
+                
+                li.addEventListener("click", () => {
+                    inputPokemon.current.value = el.name.toUpperCase();
+                    dispatch(changeID(el.url.split("https://pokeapi.co/api/v2/pokemon/")[1].split("/")[0]))//CANMBIAMOS EL 'pokemonId'(Variable Global).
+                    //OCULTAMOS TODOS LOS <li>:
+                    document.querySelectorAll('#list li').forEach((li) => {
+                        li.hidden =true;
+                    })
+                });
     
-        //LISTA DE POKEMON DEL INPUT.
-        const labelInputPokemon = document.getElementById("labelInputPokemon");
-        inputPokemon?.current.addEventListener("input", () => {//Cada que el usuario tipea en el input.
-            const listItems = document.querySelectorAll('#list li');//SELECCIONO TODOS LOS <li> del <ul>.
-            let isMatch = false;
-            listItems.forEach(li => {
-                if(li.innerText.includes(inputPokemon.current.value.toUpperCase()) && inputPokemon.current.value.length > 0){
-                    isMatch = true;
-                    return li.hidden = false;
-                }
-                return li.hidden = true;
-            })
-            
-            //HAY MATCHS?:
-            if(!isMatch){
-                //SIEMPRE QUE EL INPUT TENGA TEXTO:
-                if(inputPokemon.current.value.length > 0) labelInputPokemon.classList.remove("hiddenOpacity");
-            }else labelInputPokemon.classList.add("hiddenOpacity");
-        });
-
-    }, [totalPokemonesApi]);
+                li.hidden = true;//LOS OCULTAMOS. APARECERAN CUANDO EL USARIO TIPEE ALGO EN EL INPUT.
+    
+                //LOS AGREGAMOS AL UL:
+                ulPokemon.current.appendChild(li);
+            });
+        
+            //LISTA DE POKEMON DEL INPUT.
+            const labelInputPokemon = document.getElementById("labelInputPokemon");
+            inputPokemon?.current.addEventListener("input", () => {//Cada que el usuario tipea en el input.
+                const listItems = document.querySelectorAll('#list li');//SELECCIONO TODOS LOS <li> del <ul>.
+                let isMatch = false;
+                listItems.forEach(li => {
+                    if(li.innerText.includes(inputPokemon.current.value.toUpperCase()) && inputPokemon.current.value.length > 0){
+                        isMatch = true;
+                        return li.hidden = false;
+                    }
+                    return li.hidden = true;
+                })
+                
+                //HAY MATCHS?:
+                if(!isMatch){
+                    //SIEMPRE QUE EL INPUT TENGA TEXTO:
+                    if(inputPokemon.current.value.length > 0) labelInputPokemon.classList.remove("hiddenOpacity");
+                }else labelInputPokemon.classList.add("hiddenOpacity");
+            });
+        }
+    }, [totalPokemonesApi, loading]);
 
     //BUSCAR POKEMON POR EL INPUT:
     const navigate = useNavigate();
@@ -181,26 +183,28 @@ const Pokedex = ({credentials, pokemonTypes, }) => {
     //CARGAMOS LOS TIPOS A LA LISTA:
     const listaTipoPokemon = useRef();
     useEffect(() => {
-        for(const type of pokemonTypes){
-            if(type.name != "shadow" && type.name != "unknown"){//ESTOS 2 NO SE CARGAN YA QUE SU LISTA DE POKEMONES ESTAN VACIAS.
-                const option = document.createElement("option");//CREAMOS LA 'OPTION'.
-                option.setAttribute("value", type.name);//LE AGREGAMOS SU 'value' PARA POSTERIOR USO.
-                option.text = type.name.toUpperCase();//LE DOY UN TEXTO QUE MOSTRAR.
-                //AHORA HAY QUE AGREGARLO A LA LISTA:
-                listaTipoPokemon.current.appendChild(option);//EN MAYUSCULA.
+        if(!loading){
+            for(const type of pokemonTypes){
+                if(type.name != "shadow" && type.name != "unknown"){//ESTOS 2 NO SE CARGAN YA QUE SU LISTA DE POKEMONES ESTAN VACIAS.
+                    const option = document.createElement("option");//CREAMOS LA 'OPTION'.
+                    option.setAttribute("value", type.name);//LE AGREGAMOS SU 'value' PARA POSTERIOR USO.
+                    option.text = type.name.toUpperCase();//LE DOY UN TEXTO QUE MOSTRAR.
+                    //AHORA HAY QUE AGREGARLO A LA LISTA:
+                    listaTipoPokemon.current.appendChild(option);//EN MAYUSCULA.
+                }
             }
+    
+            //Ocultamos la lista cuando el input pierde el foco:
+            document.querySelector(".search__section").addEventListener("focusout", () => {
+                const listItems = document.querySelectorAll('#list li');
+                setTimeout(() => {
+                    listItems.forEach(li => {
+                        return li.hidden = true;
+                    })
+                }, 100);
+            });
         }
-
-        //Ocultamos la lista cuando el input pierde el foco:
-        document.querySelector(".search__section").addEventListener("focusout", () => {
-            const listItems = document.querySelectorAll('#list li');
-            setTimeout(() => {
-                listItems.forEach(li => {
-                    return li.hidden = true;
-                })
-            }, 100);
-        });
-    }, []);
+    }, [loading]);
 
     //Detectar un cambio en el filtro de pokemones:
     const selectChange = (event) => {
@@ -215,41 +219,50 @@ const Pokedex = ({credentials, pokemonTypes, }) => {
 
     return (
         <section className='pokedex'>
-            <p className='welcomeText'><span>Bienvenido {credentials},</span> aquí podras encontrar tu pokemon favorito.</p>
-            
-            <section className='search__section flex flex-column gap-01'>
-                {/* FORMULARIO DE BUSQUEDA */}
-                <form onSubmit={buscarPokemon} className='basicShadow' autoComplete="off">
-                    <label htmlFor="inputPokemon" id='labelInputPokemon' className='hiddenOpacity'>No Matchs</label>
-                    <input type="text" id='inputPokemon' ref={inputPokemon} className='input' required/>
-                    <button className='button cursor-pointer'>Buscar</button>
-                </form>
-                {/* LISTA DE RESULTADOS */}
-                <ul className='ulPokemon basicShadow' id='list' ref={ulPokemon}>
-                </ul>
-                {/* SELECCIONAR TIPO DE POKEMON */}
-                <select onChange={selectChange} ref={listaTipoPokemon} className='basicShadow cursor-pointer'>
-                    <option value="">--Filtrar por Tipo--</option>
-                </select>
-            </section>
+            {
+                !loading ?
+                    <>
+                        <p className='welcomeText'><span>Bienvenido {credentials},</span> aquí podras encontrar tu pokemon favorito.</p>
+                        
+                        <section className='search__section flex flex-column gap-01'>
+                            {/* FORMULARIO DE BUSQUEDA */}
+                            <form onSubmit={buscarPokemon} className='basicShadow' autoComplete="off">
+                                <label htmlFor="inputPokemon" id='labelInputPokemon' className='hiddenOpacity'>No Matchs</label>
+                                <input type="text" id='inputPokemon' ref={inputPokemon} className='input' required/>
+                                <button className='button cursor-pointer'>Buscar</button>
+                            </form>
+                            {/* LISTA DE RESULTADOS */}
+                            <ul className='ulPokemon basicShadow' id='list' ref={ulPokemon}>
+                            </ul>
+                            {/* SELECCIONAR TIPO DE POKEMON */}
+                            <select onChange={selectChange} ref={listaTipoPokemon} className='basicShadow cursor-pointer'>
+                                <option value="">--Filtrar por Tipo--</option>
+                            </select>
+                        </section>
 
-            {/* AQUI SE MUESTRAN LOS POKEMONES */}
-            <div className='pokemons__container'>
-                {
-                    pokemonesData?.map((pokemon, index) => <PokemonCard key={index} pokemon={pokemon}/>)
-                }
-            </div>
+                        {/* AQUI SE MUESTRAN LOS POKEMONES */}
+                        <div className='pokemons__container'>
+                            {
+                                pokemonesData?.map((pokemon, index) => <PokemonCard key={index} pokemon={pokemon}/>)
+                            }
+                        </div>
 
-            {/* PAGINATION */}
-            <Pagination 
-                limit={limit} 
-                setLimit={setLimit} 
-                offset={offset} 
-                setOffset={setOffset} 
-                //           se filtra por tipo                                                  se busca de modo normal
-                totalPaginas={pokemones?.length > limit ? Math.ceil(pokemones?.length / limit) : Math.ceil
-                (totalPokemonesApi?.length / limit)}
-            />
+                        {/* PAGINATION */}
+                        <Pagination 
+                            limit={limit} 
+                            setLimit={setLimit} 
+                            offset={offset} 
+                            setOffset={setOffset} 
+                            //           se filtra por tipo                                                  se busca de modo normal
+                            totalPaginas={pokemones?.length > limit ? Math.ceil(pokemones?.length / limit) : Math.ceil
+                            (totalPokemonesApi?.length / limit)}
+                        />
+                    </>
+                :
+                    <>
+                        <i className='bx bx-loader bx-spin bx-lg' ></i>
+                    </>
+            }
         </section>
     )
 }
